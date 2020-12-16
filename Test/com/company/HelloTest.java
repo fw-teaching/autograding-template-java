19e9b8e224d21bbeec25c95a6396a58c15258497
Fredrik Welander <fredrik.welander@subsite.fi>
2020-12-16 23:10:44 +0200

    Add gradle testing

---
+++ Test/com/company/HelloTest.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+    @Test
+    public void getName() throws Exception {
+        Hello h = new Hello("testname");
+        assertEquals("testname", h.getName());
+    }
+}
