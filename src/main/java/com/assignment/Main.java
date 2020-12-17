4081c9fba6cc1d72d6c8466c3583f26878f69245
Fredrik Welander <fredrik.welander@subsite.fi>
2020-12-17 16:18:44 +0200

    Flush solution code

--- src/main/java/com/assignment/Main.java
+++ src/main/java/com/assignment/Main.java
@@ -3,9 +3,6 @@ package com.assignment;
 public class Main {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello("fredde");
-
-		System.out.println("hello " + hello.getName());
 
 	}
 }
