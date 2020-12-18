9b34323e2fa8afed75138c0b69040ea91e5925d1
Fredrik Welander <fredrik.welander@subsite.fi>
2020-12-18 21:06:46 +0200

    Fix dynamic class loading

--- src/test/java/com/assignment/HelloTest.java
+++ src/test/java/com/assignment/HelloTest.java
@@ -5,29 +5,36 @@
 package com.assignment;
 
 import org.junit.jupiter.api.Test;
-
 import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Testa Hello-klassens instansiering och getter
+ * Testar Hello-klassens instansiering och getter
  * Kör testet: gradle test --tests HelloTest
  */
 class HelloTest {
+
     @Test
     public void getName() {
+
+        String out = null;
 
         /**
          *  Dynamisk version av: Hello h = new Hello("testname");
          *  Behövs för att testet skall kunna kompilera fast Hello-klassen inte existerar
+         *  (gradle kompilerar alla test fast man inte skulle köra alla)
          */
-        Class h = null;
         try {
-            h = (Class) Class.forName("Hello").getDeclaredConstructor().newInstance("testname");
-        } catch (Exception e) {
+            Class<?> cls = Class.forName("com.assignment.Hello");
+            Object obj = cls.getConstructor(String.class).newInstance("testname");
+            out = cls.getDeclaredMethod("getName").invoke(obj).toString();
+
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        assertEquals("testname", h.getName());
+        assertEquals("testname", out);
+
     }
+
 }
